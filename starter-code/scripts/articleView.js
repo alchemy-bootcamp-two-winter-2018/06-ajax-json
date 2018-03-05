9d9b888875c059f6b59c1d579735d9bbb21f0182
Victor Bofill <terranvas@gmail.com>
2018-03-05 13:33:45 -0800

    Initial lint pass

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -106,20 +106,19 @@ articleView.create = () => {
 };
 
 articleView.renderArticles = function(articles) {
-    articles.forEach(article => {
-      $('#articles').append(article.toHtml())
-    });
+  articles.forEach(article => {
+    $('#articles').append(article.toHtml())
   });
-}
+};
 
 // REVIEW: This function will retrieve the data from either a local or remote source
 articleView.fetchAll = () => {
   // TODO:
   // 1) make an AJAX call to the server for the raw data
-  // 2) ASYNCHRONOUSLY (use .then) 
-      // A) call Article.loadAll with the data you got from the server and get array of Article objects
-      // B) call renderArticles to put the article object into the DOM
-      // C) call setupView method to finish wiring up the UI for things that need the data to be loaded 
+  // 2) ASYNCHRONOUSLY (use .then)
+  // A) call Article.loadAll with the data you got from the server and get array of Article objects
+  // B) call renderArticles to put the article object into the DOM
+  // C) call setupView method to finish wiring up the UI for things that need the data to be loaded
 }
 
 articleView.setupView = () => {
